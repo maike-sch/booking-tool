e65034fd8c0287178f42f6c605644309d0d846ee
Marc Aschmann <marc@aschmann.org>
2025-11-26 19:14:48 +0100

    Revert "more styling"
    This reverts commit 2b32a7f42ec35aa5506e336b19aa1830dd5b1516.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -11,7 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, errorLoggingInterceptor])),
-    provideAnimations()
+    provideHttpClient(withInterceptors([authInterceptor, errorLoggingInterceptor]))
   ]
 };
