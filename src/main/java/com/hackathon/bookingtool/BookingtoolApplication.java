9d0f9907cfce2a7f04a4ee09a93b6d96c68ebaf8
schwarzmueller <maike.schwarzmueller@adesso.de>
2025-11-26 16:51:00 +0100

    spring initializr

---
+++ src/main/java/com/hackathon/bookingtool/BookingtoolApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.bookingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/bookingtool/BookingtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.bookingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
